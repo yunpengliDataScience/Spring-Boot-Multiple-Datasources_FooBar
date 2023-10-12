dad76a4c8dbcf68b5c1026679307c500422e9697
Yunpeng Li <54851187+yunpengliDataScience@users.noreply.github.com>
2023-10-12 13:23:41 -0400

    Update FooBarController.java

--- src/main/java/com/foobar/FooBarController.java
+++ src/main/java/com/foobar/FooBarController.java
@@ -1,5 +1,7 @@
 package com.foobar;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,21 +15,67 @@ import com.foobar.foo.repo.FooRepository;
 @RestController
 public class FooBarController {
 
-  private final FooRepository fooRepo;
-  private final BarRepository barRepo;
+	private final FooRepository fooRepo;
+	private final BarRepository barRepo;
 
-  @Autowired
-  FooBarController(FooRepository fooRepo, BarRepository barRepo) {
-    this.fooRepo = fooRepo;
-    this.barRepo = barRepo;
-  }
+	@Autowired
+	FooBarController(FooRepository fooRepo, BarRepository barRepo) {
+		this.fooRepo = fooRepo;
+		this.barRepo = barRepo;
+	}
 
-  @RequestMapping("/foobar/{id}")
-  public String fooBar(@PathVariable("id") Long id) {
-    Foo foo = fooRepo.findById(id);
-    Bar bar = barRepo.findById(id);
+	@RequestMapping("/foobar")
+	public String all() {
+		List<Foo> foos = fooRepo.findAll();
+		List<Bar> bars = barRepo.findAll();
 
-    return foo.getFoo() + " " + bar.getBar() + "!";
-  }
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("Foos: [");
+		for (Foo foo : foos) {
+			builder.append(foo.getFoo() + " ");
+		}
+		builder.append("]");
+
+		builder.append(" Bars: [");
+		for (Bar bar : bars) {
+			builder.append(bar.getBar() + " ");
+		}
+		builder.append("]");
+
+		return builder.toString();
+	}
+
+	@RequestMapping("/foobar/{id}")
+	public String fooBar(@PathVariable("id") Long id) {
+
+		System.out.println("aaa");
+
+		Foo foo = new Foo();
+		if (fooRepo.findById(id).isPresent()) {
+			foo = fooRepo.findById(id).get();
+		}
+
+		Bar bar = new Bar();
+		if (barRepo.findById(id).isPresent()) {
+			bar = barRepo.findById(id).get();
+		}
+
+		return foo.getFoo() + " " + bar.getBar() + "!";
+	}
+
+	@RequestMapping("/foobar/create/{number}")
+	public String create(@PathVariable("number") Long number) {
+
+		System.out.println("bbb");
+
+		Foo foo = new Foo("Foo" + number);
+		fooRepo.save(foo);
+
+		Bar bar = new Bar("Bar" + number);
+		barRepo.save(bar);
+
+		return "Created " + foo.getFoo() + " " + bar.getBar() + "!";
+	}
 
 }
